91a66dbdd83a85eda62199a30d10d38f10270956
Schemiii <SchemiiiDe@googlemail.com>
2012-11-08 10:44:10 +0100

    Blatt3_U1_Triangle

---
+++ iOs/Triangle/Triangle/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Triangle
+//
+//  Created by Daniel Schmidt on 08.11.12.
+//  Copyright (c) 2012 Daniel Schmidt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
